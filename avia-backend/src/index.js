07d9ca30f67e97ac82cb6578bcbb25671361d037
Deviatov Ilya <deviatovilya@MacBook-Air-cua-Deviatov.local>
2026-05-21 11:34:10 +0300

    Lab 4: static public bundle added successfully

--- avia-backend/src/index.js
+++ avia-backend/src/index.js
@@ -1,32 +1,16 @@
 const express = require('express');
-const cors = require('cors');
 const path = require('path');
-const flightsRouter = require('./routes/flights');
-const flightsService = require('./services/flightsService');
-
 const app = express();
 const PORT = 3000;
 
-const DATA_FILE_PATH = path.join(__dirname, 'data/flights.json');
-flightsService.init(DATA_FILE_PATH);
-
-app.use(cors());
 app.use(express.json());
 
-app.use((req, res, next) => {
-    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
-    next();
-});
+app.use(express.static(path.join(__dirname, '../public')));
 
-app.use('/api/flights', flightsRouter);
+app.use('/api/flights', require('./routes/flights'));
 
-app.use((req, res) => {
-    res.status(404).json({ error: 'Маршрут не найден' });
-});
-
-app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500).json({ error: 'Внутренняя ошибка сервера' });
+app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 app.listen(PORT, () => {
